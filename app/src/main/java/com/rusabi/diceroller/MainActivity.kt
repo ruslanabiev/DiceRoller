ee20b7f4bef16ffd39c47217c0e1b1b71132ce83
Ruslan Abiev <abievruslan@yandex.ru>
2022-05-21 11:25:17 +0300

    Create the layout infrastructure

--- app/src/main/java/com/rusabi/diceroller/MainActivity.kt
+++ app/src/main/java/com/rusabi/diceroller/MainActivity.kt
@@ -4,40 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.rusabi.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            DiceRollerApp()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DiceRollerTheme {
-        Greeting("Android")
-    }
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+
 }
